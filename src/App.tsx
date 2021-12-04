35f2819212efbd4de209a5bb4b53cc5d28462fe3
vahidGhadiri <whydrf@gmail.com>
2021-12-04 23:09:33 +0330

    Basic Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import {Route, Switch} from "react-router";
-import {Layout} from "antd";
+import {Layout, Space, Typography} from "antd";
 
 import {Cryptocurrencies, CryptoDetail, Exchanges, Home, Navbar, News} from "./components"
 
 import "./App.css"
+import {Link} from "react-router-dom";
 
 const App: React.FC = () => {
     return (
@@ -24,8 +25,18 @@ const App: React.FC = () => {
                         </Switch>
                     </div>
                 </Layout>
+                <div className="footer">
+                    <Typography.Title level={5} style={{color: "#fff", textAlign: "center"}}>
+                        Cryptoverse <br/>
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news ">News</Link>
+                    </Space>
+                </div>
             </div>
-            <div className="footer">footer</div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.component.tsx
+++ src/components/Navbar/Navbar.component.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = (): JSX.Element => {
         <div className="nav-container">
             <div className="logo-container">
                 <Avatar src={icon} size="large"/>
-                <Typography.Title level={2} className="logo"><Link to={"/"}>Crytoverse</Link></Typography.Title>
+                <Typography.Title level={2} className="logo"><Link to={"/"}> </Link></Typography.Title>
                 <Menu theme="dark">
                     <Menu.Item icon={<HomeOutlined/>}>
                         <Link to={"/"}>Home</Link>

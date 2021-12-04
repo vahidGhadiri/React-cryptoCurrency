f19b62bd0cec3cd6db90b9732777a5e8d0005053
vahidGhadiri <whydrf@gmail.com>
2021-12-04 22:12:20 +0330

    Basic Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react"
+import {Navbar} from "./components"
+
+import "./App.css"
 
 const App: React.FC = () => {
     return (
-        <div>App</div>
+        <div className="app">
+            <div className="navbar">
+                <Navbar/>
+            </div>
+            <div className="main">main</div>
+            <div className="footer">footer</div>
+        </div>
     )
 }
 

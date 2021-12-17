88f0cc6c35c942a375d4ace8a49e1caf4c2725ba
vahidGhadiri <whydrf@gmail.com>
2021-12-17 13:54:53 +0330

    Home page

--- src/components/Home/Home.componsnt.tsx
+++ src/components/Home/Home.componsnt.tsx
@@ -1,10 +1,19 @@
 import React from "react"
+import {Col, Row, Statistic, Typography} from "antd";
+
 
 const Home: React.FC = (): JSX.Element => {
     return (
-        <div>
-            Home page
-        </div>
+        <>
+            <Typography.Title level={2} className="heading ">Global Crypto Stats</Typography.Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={5}/></Col>
+                <Col span={12}><Statistic title="Total Exchanges " value={5}/></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value={5}/></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value={5}/></Col>
+                <Col span={12}><Statistic title="Total Markets" value={5}/></Col>
+            </Row>
+        </>
     )
 }
 

9d49151e9f9ee54e28f4e8f10e35153524765760
vahidGhadiri <whydrf@gmail.com>
2021-12-18 20:05:22 +0330

    Home Component

--- src/components/Home/Home.component.tsx
+++ src/components/Home/Home.component.tsx
@@ -1,16 +1,10 @@
 import React from "react"
-import {connect} from "react-redux"
 import {Col, Row, Statistic, Typography} from "antd";
-
-import {getExchanges} from "../../redux/actions/exchanges.action";
-import {getMarkets} from "../../redux/actions/markets.action";
 
 
 const Home: React.FC<any> = (props): JSX.Element => {
     return (
         <>
-            <button
-                onClick={() => props.getMarkets()}>{props.marketsLoading ? "IS LOADING..." : "CALL MARKETS API"}</button>
             <Typography.Title level={2} className="heading ">Global Crypto Stats</Typography.Title>
             <Row>
                 <Col span={12}><Statistic title="Total Cryptocurrencies" value={5}/></Col>
@@ -23,15 +17,5 @@ const Home: React.FC<any> = (props): JSX.Element => {
     )
 }
 
-const mapStateToProps = (state: any) => ({
-    exchanges: state.exchange.exchanges,
-    exchangesLoading: state.exchange.isLoading,
-    marketsLoading: state.market.isLoading
-})
 
-const mapDispatchToProps = (dispatch: any) => ({
-    getExchanges: () => dispatch(getExchanges()),
-    getMarkets: () => dispatch(getMarkets())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default Home

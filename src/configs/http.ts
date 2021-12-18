639a4765216b4a46f965d28896a1849d4ca5a364
vahidGhadiri <whydrf@gmail.com>
2021-12-18 20:57:34 +0330

    Adding interceptors to custom axios

--- src/configs/http.ts
+++ src/configs/http.ts
@@ -5,5 +5,22 @@ export const http = axios.create({
     headers: {
         'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
         'x-rapidapi-key': '69a238c52cmshd0000458a91563ep1cb71cjsn5b681975faa3'
-    }
+    },
+    timeout: 10000
 })
+
+http.interceptors.request.use(
+    (request) => {
+        console.log("axios-request", request)
+        return request
+    },
+    (error) => console.log(error)
+)
+
+http.interceptors.response.use(
+    (response) => {
+        console.log("axios-response", response)
+        return response
+    },
+    (error) => console.log(error)
+)

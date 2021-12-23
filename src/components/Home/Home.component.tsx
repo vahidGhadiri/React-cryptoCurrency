61820aa2c011bb21ab39355f19b7b362058f0b1f
vahidGhadiri <whydrf@gmail.com>
2021-12-23 23:14:40 +0330

    chore: change structure

--- src/components/Home/Home.component.tsx
+++ src/components/Home/Home.component.tsx
@@ -1,17 +1,16 @@
 import React from "react"
 import {connect} from "react-redux";
 import {Col, Row, Statistic, Typography} from "antd";
-
-import {getExchanges} from "../../redux/actions/exchanges.action";
-import {getCoin, getCoins} from "../../redux/actions/coins.action";
-import {StoreInterface} from "../../redux/store/store";
+import exchangesActions from "../../store/exchanges/exchanges.actions";
+import coinsActions from "../../store/coins/coins.actions";
+import marketsActions from "../../store/markets/markets.action";
 
 
 const Home: React.FC<any> = (props): JSX.Element => {
     return (
         <>
             <Typography.Title level={2} className="heading ">Global Crypto Stats</Typography.Title>
-            <button onClick={() => props.getExchanges()}>GET COINS</button>
+            <button onClick={() => props.getExchanges()}>{!props.isLoading ? "Call the services" : "please wait"}</button>
             <Row>
                 <Col span={12}><Statistic title="Total Cryptocurrencies" value={5}/></Col>
                 <Col span={12}><Statistic title="Total Exchanges " value={5}/></Col>
@@ -23,15 +22,14 @@ const Home: React.FC<any> = (props): JSX.Element => {
     )
 }
 
-
-const mapStateToProps = (state: StoreInterface) => ({
-    exchanges: state.exchange.exchanges
+const mapStateToProps = (state: any) => ({
+    isLoading: state.exchanges.isLoading
 })
 
 const mapDispatchToProps = (dispatch: any) => ({
-    getExchanges: () => dispatch(getExchanges()),
-    getCoins: () => dispatch(getCoins()),
-    getCoin: () => dispatch(getCoin())
+    getExchanges: () => dispatch(exchangesActions.getExchanges()),
+    getCoins: () => dispatch(coinsActions.getCoins()),
+    getMarkets: () => dispatch(marketsActions.getMarkets())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)

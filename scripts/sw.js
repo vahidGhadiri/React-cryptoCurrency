86c8da22ef317b4902851da92a31444fbc944dcc
vahidGhadiri <whydrf@gmail.com>
2022-01-13 11:13:04 +0330

    adding service workers

--- scripts/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", (event) => {
-    console.log("[service-worker]: is installed...", event)
-})
-
-self.addEventListener("activate", (event) => {
-    console.log("[service-worker]: is activated...", event)
-})
